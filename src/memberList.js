511de49e4871b113af7a02f62eced823b3a6fee2
BbyfoxY <77471733+BbyfoxY@users.noreply.github.com>
2021-12-02 07:47:34 -0500

    Here we go

---
+++ src/memberList.js
@@ -0,0 +1,17 @@
+/* SECRET MEMBER LIST
+        WE F. | RANK: GARDENER
+        AL L. | RANK: S1
+        ST D. | RANK: S10
+        JA S. | RANK: B4
+        PA D. | RANK: B1
+        HE S. | RANK: A9
+        AL F. | RANK: B2
+        NA S. | RANK: C2
+        JO K. | RANK: S6
+        AV J. | RANK: S8
+        CO F. | RANK: S7
+        EM H. | RANK: A4
+        MA H. | RANK: A1
+        PH D. | RANK: S5
+        GA B. | RANK: B7
+*/
