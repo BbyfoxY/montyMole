c1dabe9c0274bb83504b1919dc66ea24dcf5ca7d
BbyfoxY <77471733+BbyfoxY@users.noreply.github.com>
2021-12-02 09:09:49 -0500

    Hope works

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-
+//variables
+var srcM = document.getElementById("srcM");
+var srcD = document.getElementById("srcD");
+var srcE = document.getElementById("srcE");
+var srcG = document.getElementById("srcG");
+var srcS = document.getElementById("srcS");
+        
+var montyImage = [
+  srcM,
+  srcD,
+  srcE,
+  srcG,
+  srcS
+];
+//functions
+function montyCreate() {
+  var montyCreation = document.createElement("img");
+  montyCreation.src = montyImage[Math.floor(Math.random() * 4)];
+  montyCreation.width = "50";
+  montyCreation.length = "50";
+  document.getElementById("montyCreationBody").appendChild(montyCreation);
+}
